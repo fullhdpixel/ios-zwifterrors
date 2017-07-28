66a22cb9db1b5f0aa05c00b61ce5c26ecf1fdaef
fullhdpixel <tsmudde@hotmail.com>
2017-07-29 01:29:27 +0200

    first commit

---
+++ mobile-config.js
@@ -0,0 +1,12 @@
+// This section sets up some basic app metadata, the entire section is optional.
+App.info({
+  id: "com.zwifterrors",
+  name: "ios-zwifterrors",
+  description: "",
+  version: "1.0.0",
+  buildNumber: "1"
+});
+
+App.configurePlugin("cordova-plugin-googleplus", {
+  REVERSED_CLIENT_ID: "com.googleusercontent.apps.678272593084-i358gt3fddf9arhdo551m2v1mnq7po9f"
+});
